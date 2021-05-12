97585cf24b34d854ecbeb58a294ba40b550bca95
Giovanni Pantuzzo <giovannipantuzzo@cpejr.com.br>
2021-05-12 14:01:09 -0300

    Controller
    alterações no models

---
+++ src/models/UserModel.js
@@ -0,0 +1,30 @@
+const connection = require("../database/connection");
+const { v4: uuidv4 } = require("uuid");
+
+module.exports = {
+  async create(user) {
+    const user_id = uuidv4();
+    user.user_id = user_id;
+
+    await connection("user").insert(user);
+    return user_id;
+  },
+
+  async getById({ user_id }) {
+    const result = await connection("user")
+      .where({ user_id })
+      .select("*")
+      .first();
+    return result;
+  },
+
+  async updateById(user, user_id) {
+    const result = await connection("user").where({ user_id }).update(user);
+    return result;
+  },
+
+  async deleteById(user_id) {
+    const result = await connection("user").where({ user_id }).delete();
+    return result;
+  },
+};
