3fa625a58f63cc5c9aa96b0df1e7b6c156190e9e
Giovanni Pantuzzo <giovannipantuzzo@cpejr.com.br>
2021-05-13 21:16:33 -0300

    correção login firebase

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+const { errors } = require("celebrate");
 const express = require("express");
 const cors = require("cors");
 
@@ -8,6 +9,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.unsubscribe(errors());
 
 app.listen(port, () => {
   console.log("Server listening on port: " + port);
