d6fd433a0d904b8202294560ace5c586dc0a8303
Giovanni Pantuzzo <giovannipantuzzo@cpejr.com.br>
2021-05-13 21:17:05 -0300

    correção

--- src/utils/Firebase.js
+++ src/utils/Firebase.js
@@ -1,29 +1,30 @@
-cons firebase = require("firebase/app");
+const firebase = require("firebase/app");
 require("firebase/auth");
-
 
 const firebaseConfig = {
   apiKey: process.env.APIKEY,
   authDomain: process.env.AUTHDOMAIN,
   projectId: process.env.PROJECT_ID,
   storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
 };
 
-  firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
 
-  module.exports = {
-    async createNewUser(email, password) {
+module.exports = {
+  async createNewUser(email, password) {
+    const result = await firebase
+      .auth()
+      .createUserWithEmailandPassword(email, password);
 
-        const result = await firebase.auth().createUserWithEmailandPassword(email, password);
+    return result.user.uid;
+  },
 
-        return result.user.uid;
-    },
+  async login(email, password) {
+    const result = await firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password);
 
-    async login(email, password) {
-
-        const result = await firebase.auth().signInWithEmailAndPassword(email, password);
-
-        return result.user.uid;
-    },
-  };
+    return result.user.uid;
+  },
+};
